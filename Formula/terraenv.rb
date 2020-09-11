a33a7e2ca747b2e4ce21f9a99c8c36fea6cb5e41
Aarat Nathwani <me@aarat.com>
2020-09-11 16:17:24 +0530

    Update terraenv.rb

--- Formula/terraenv.rb
+++ Formula/terraenv.rb
@@ -1,11 +1,11 @@
 class Terraenv < Formula
-  TERRAENV_VERSION = "v0.10".freeze
+  TERRAENV_VERSION = "v0.11".freeze
 
   desc "Installs Terraenv from github"
   homepage "https://github.com/aaratn/terraenv"
   url "https://github.com/aaratn/terraenv/releases/download/#{TERRAENV_VERSION}/terraenv_osx_x64.tar.gz"
   version #{TERRAENV_VERSION}
-  sha256 '571538b3cf4ea6783e7a480ef92a158eb1e314dac8f047c57f35087ea6bf4d84'
+  sha256 '1f0453785a3cff4de87d742a07894d94a0a73579a6d3cbafc351b9b03a53cb87'
 
   def install
     bin.install 'terraenv'
